300a1df12dad76383acbf8ee2dce2343adc93598
macalimlim <macalimlim@gmail.com>
2013-09-05 04:49:29 +0800

    added User and Room entity classes

---
+++ src/net/dlogic/kryonet/common/entity/Room.java
@@ -0,0 +1,6 @@
+package net.dlogic.kryonet.common.entity;
+
+public class Room {
+	public String name;
+	public User[] users;
+}
---
+++ src/net/dlogic/kryonet/common/entity/User.java
@@ -0,0 +1,6 @@
+package net.dlogic.kryonet.common.entity;
+
+public class User {
+	public String username;
+	public Room[] joinedRooms;
+}
