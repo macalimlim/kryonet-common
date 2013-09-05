566c94b71519724554d2e8df5bca0deed79245c9
macalimlim <macalimlim@gmail.com>
2013-09-05 10:21:14 +0800

    added a target Room

--- src/net/dlogic/kryonet/common/request/PublicMessageRequest.java
+++ src/net/dlogic/kryonet/common/request/PublicMessageRequest.java
@@ -1,5 +1,8 @@
 package net.dlogic.kryonet.common.request;
 
+import net.dlogic.kryonet.common.entity.Room;
+
 public class PublicMessageRequest {
 	public String message;
+	public Room targetRoom;
 }
