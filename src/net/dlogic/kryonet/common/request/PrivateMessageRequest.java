9c3b31945a4406b9ce5135d2a7db262cb651d882
macalimlim <macalimlim@gmail.com>
2013-09-05 10:19:37 +0800

    added PrivateMessage request and response classes

---
+++ src/net/dlogic/kryonet/common/request/PrivateMessageRequest.java
@@ -0,0 +1,8 @@
+package net.dlogic.kryonet.common.request;
+
+import net.dlogic.kryonet.common.entity.User;
+
+public class PrivateMessageRequest {
+	public String message;
+	public User targetUser;
+}
---
+++ src/net/dlogic/kryonet/common/response/PrivateMessageResponse.java
@@ -0,0 +1,8 @@
+package net.dlogic.kryonet.common.response;
+
+import net.dlogic.kryonet.common.entity.User;
+
+public class PrivateMessageResponse {
+	public String message;
+	public User fromUser;
+}
