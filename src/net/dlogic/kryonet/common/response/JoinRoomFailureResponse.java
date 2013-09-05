5919611819f8832c65864a55cd8b654dbe10c0ce
macalimlim <macalimlim@gmail.com>
2013-09-05 22:27:00 +0800

    added JoinRoomFailureResponse and JoinRoomSuccessResponse classes

---
+++ src/net/dlogic/kryonet/common/response/JoinRoomFailureResponse.java
@@ -0,0 +1,5 @@
+package net.dlogic.kryonet.common.response;
+
+public class JoinRoomFailureResponse {
+	public String errorMessage;
+}
---
+++ src/net/dlogic/kryonet/common/response/JoinRoomSuccessResponse.java
@@ -0,0 +1,7 @@
+package net.dlogic.kryonet.common.response;
+
+import net.dlogic.kryonet.common.entity.Room;
+
+public class JoinRoomSuccessResponse {
+	public Room joinedRoom;
+}
