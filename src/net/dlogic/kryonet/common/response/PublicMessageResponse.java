bfecda1bbd7f1d76e8f43eca2163feb5d78d7114
macalimlim <macalimlim@gmail.com>
2013-09-05 10:23:33 +0800

    added a from User

--- src/net/dlogic/kryonet/common/response/PublicMessageResponse.java
+++ src/net/dlogic/kryonet/common/response/PublicMessageResponse.java
@@ -1,5 +1,8 @@
 package net.dlogic.kryonet.common.response;
 
+import net.dlogic.kryonet.common.entity.User;
+
 public class PublicMessageResponse {
+	public User fromUser;
 	public String message;
 }
