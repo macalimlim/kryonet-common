20ed953feabc60ccb21bb4d1a8515158f2d656a7
macalimlim <macalimlim@gmail.com>
2013-09-06 09:28:02 +0800

    added a KryonetUtility class to register request and response classes

---
+++ src/net/dlogic/kryonet/common/utility/KryonetUtility.java
@@ -0,0 +1,35 @@
+package net.dlogic.kryonet.common.utility;
+
+import net.dlogic.kryonet.common.request.JoinRoomRequest;
+import net.dlogic.kryonet.common.request.LoginRequest;
+import net.dlogic.kryonet.common.request.LogoutRequest;
+import net.dlogic.kryonet.common.request.PrivateMessageRequest;
+import net.dlogic.kryonet.common.request.PublicMessageRequest;
+import net.dlogic.kryonet.common.response.JoinRoomFailureResponse;
+import net.dlogic.kryonet.common.response.JoinRoomSuccessResponse;
+import net.dlogic.kryonet.common.response.LoginFailureResponse;
+import net.dlogic.kryonet.common.response.LoginSuccessResponse;
+import net.dlogic.kryonet.common.response.LogoutResponse;
+import net.dlogic.kryonet.common.response.PrivateMessageResponse;
+import net.dlogic.kryonet.common.response.PublicMessageResponse;
+
+import com.esotericsoftware.kryonet.EndPoint;
+
+public class KryonetUtility {
+	public static void registerClasses(EndPoint endpoint) {
+		//Request classes
+		endpoint.getKryo().register(JoinRoomRequest.class);
+		endpoint.getKryo().register(LoginRequest.class);
+		endpoint.getKryo().register(LogoutRequest.class);
+		endpoint.getKryo().register(PrivateMessageRequest.class);
+		endpoint.getKryo().register(PublicMessageRequest.class);
+		//Response classes
+		endpoint.getKryo().register(JoinRoomFailureResponse.class);
+		endpoint.getKryo().register(JoinRoomSuccessResponse.class);
+		endpoint.getKryo().register(LoginFailureResponse.class);
+		endpoint.getKryo().register(LoginSuccessResponse.class);
+		endpoint.getKryo().register(LogoutResponse.class);
+		endpoint.getKryo().register(PrivateMessageResponse.class);
+		endpoint.getKryo().register(PublicMessageResponse.class);
+	}
+}
