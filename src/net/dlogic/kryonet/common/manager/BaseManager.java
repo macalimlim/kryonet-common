926e130a75ee1856925f8c8472a0f9234b9fbf2f
macalimlim <macalimlim@gmail.com>
2013-09-09 21:35:36 +0800

    added BaseManager class for UserManager and RoomManager classes

---
+++ src/net/dlogic/kryonet/common/manager/BaseManager.java
@@ -0,0 +1,16 @@
+package net.dlogic.kryonet.common.manager;
+
+import java.util.Map;
+
+public class BaseManager<V> {
+	private Map<Integer, V> map;
+	public V put(Integer key, V value) {
+		return map.put(key, value);
+	}
+	public V remove(int key) {
+		return map.remove(key);
+	}
+	public V get(int key) {
+		return map.get(key);
+	}
+}
