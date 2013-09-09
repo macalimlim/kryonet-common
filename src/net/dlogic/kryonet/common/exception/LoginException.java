f2157085b24686edabddc050a01110d509b5d7b6
macalimlim <macalimlim@gmail.com>
2013-09-09 21:35:08 +0800

    added LoginException class for invalid logins

---
+++ src/net/dlogic/kryonet/common/exception/LoginException.java
@@ -0,0 +1,8 @@
+package net.dlogic.kryonet.common.exception;
+
+public class LoginException extends Exception {
+	private static final long serialVersionUID = 8141032520444561308L;
+	public LoginException(String message) {
+		super(message);
+	}
+}
