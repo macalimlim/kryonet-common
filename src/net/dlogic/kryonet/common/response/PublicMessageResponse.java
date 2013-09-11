4422ad9a1e1f1278a5817b9455e44574dc3a68c8
macalimlim <macalimlim@gmail.com>
2013-09-11 09:00:02 +0800

    changed from 'fromUser' to 'sender'

--- src/net/dlogic/kryonet/common/response/PublicMessageResponse.java
+++ src/net/dlogic/kryonet/common/response/PublicMessageResponse.java
@@ -3,6 +3,6 @@ package net.dlogic.kryonet.common.response;
 import net.dlogic.kryonet.common.entity.User;
 
 public class PublicMessageResponse {
-	public User fromUser;
+	public User sender;
 	public String message;
 }
