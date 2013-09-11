f1c1ddedf760e074c6e6374bfa367fc05e266e63
macalimlim <macalimlim@gmail.com>
2013-09-12 01:57:26 +0800

    added roomToLeave property

--- src/net/dlogic/kryonet/common/response/LeaveRoomResponse.java
+++ src/net/dlogic/kryonet/common/response/LeaveRoomResponse.java
@@ -1,7 +1,9 @@
 package net.dlogic.kryonet.common.response;
 
+import net.dlogic.kryonet.common.entity.Room;
 import net.dlogic.kryonet.common.entity.User;
 
 public class LeaveRoomResponse {
 	public User userToLeave;
+	public Room roomToLeave;
 }
