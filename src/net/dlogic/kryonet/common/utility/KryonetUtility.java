7532b1625dd9d6782fc6958dd1853aa4ff3fe9a5
macalimlim <macalimlim@gmail.com>
2013-09-12 01:09:53 +0800

    registered the LeaveRoomRequest anmd LeaveRoomResponse classes

--- src/net/dlogic/kryonet/common/utility/KryonetUtility.java
+++ src/net/dlogic/kryonet/common/utility/KryonetUtility.java
@@ -1,12 +1,14 @@
 package net.dlogic.kryonet.common.utility;
 
 import net.dlogic.kryonet.common.request.JoinRoomRequest;
+import net.dlogic.kryonet.common.request.LeaveRoomRequest;
 import net.dlogic.kryonet.common.request.LoginRequest;
 import net.dlogic.kryonet.common.request.LogoutRequest;
 import net.dlogic.kryonet.common.request.PrivateMessageRequest;
 import net.dlogic.kryonet.common.request.PublicMessageRequest;
 import net.dlogic.kryonet.common.response.JoinRoomFailureResponse;
 import net.dlogic.kryonet.common.response.JoinRoomSuccessResponse;
+import net.dlogic.kryonet.common.response.LeaveRoomResponse;
 import net.dlogic.kryonet.common.response.LoginFailureResponse;
 import net.dlogic.kryonet.common.response.LoginSuccessResponse;
 import net.dlogic.kryonet.common.response.LogoutResponse;
@@ -19,6 +21,7 @@ public class KryonetUtility {
 	public static void registerClasses(EndPoint endpoint) {
 		//Request classes
 		endpoint.getKryo().register(JoinRoomRequest.class);
+		endpoint.getKryo().register(LeaveRoomRequest.class);
 		endpoint.getKryo().register(LoginRequest.class);
 		endpoint.getKryo().register(LogoutRequest.class);
 		endpoint.getKryo().register(PrivateMessageRequest.class);
@@ -26,6 +29,7 @@ public class KryonetUtility {
 		//Response classes
 		endpoint.getKryo().register(JoinRoomFailureResponse.class);
 		endpoint.getKryo().register(JoinRoomSuccessResponse.class);
+		endpoint.getKryo().register(LeaveRoomResponse.class);
 		endpoint.getKryo().register(LoginFailureResponse.class);
 		endpoint.getKryo().register(LoginSuccessResponse.class);
 		endpoint.getKryo().register(LogoutResponse.class);
