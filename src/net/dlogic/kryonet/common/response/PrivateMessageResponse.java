da0e609c4b79707c19625ac91204a2e826b14efd
macalimlim <macalimlim@gmail.com>
2013-09-11 08:59:28 +0800

    reordered the properties

--- src/net/dlogic/kryonet/common/response/PrivateMessageResponse.java
+++ src/net/dlogic/kryonet/common/response/PrivateMessageResponse.java
@@ -3,6 +3,6 @@ package net.dlogic.kryonet.common.response;
 import net.dlogic.kryonet.common.entity.User;
 
 public class PrivateMessageResponse {
+	public User sender;
 	public String message;
-	public User fromUser;
 }
