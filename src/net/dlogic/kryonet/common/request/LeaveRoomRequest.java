d96dd2dc4862ffbc054cbf0f730d194a61054af7
macalimlim <macalimlim@gmail.com>
2013-09-12 01:09:09 +0800

    added LeaveRoomRequest and LeaveRoomResponse classes

---
+++ src/net/dlogic/kryonet/common/request/LeaveRoomRequest.java
@@ -0,0 +1,7 @@
+package net.dlogic.kryonet.common.request;
+
+import net.dlogic.kryonet.common.entity.Room;
+
+public class LeaveRoomRequest {
+	public Room roomToLeave;
+}
---
+++ src/net/dlogic/kryonet/common/response/LeaveRoomResponse.java
@@ -0,0 +1,7 @@
+package net.dlogic.kryonet.common.response;
+
+import net.dlogic.kryonet.common.entity.User;
+
+public class LeaveRoomResponse {
+	public User userToLeave;
+}
