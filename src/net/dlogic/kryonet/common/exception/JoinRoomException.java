f19da4e7fec4fbd2645b278dec7255b9adf0d3df
macalimlim <macalimlim@gmail.com>
2013-09-12 09:35:26 +0800

    added JoinRoomException class

---
+++ src/net/dlogic/kryonet/common/exception/JoinRoomException.java
@@ -0,0 +1,8 @@
+package net.dlogic.kryonet.common.exception;
+
+public class JoinRoomException extends Exception {
+	private static final long serialVersionUID = 1263508656914467466L;
+	public JoinRoomException(String message) {
+		super(message);
+	}
+}
