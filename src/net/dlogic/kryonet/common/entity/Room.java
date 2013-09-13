98edcbb3f0a661f7a6fea968ef8e7f35df5d7b71
macalimlim <macalimlim@gmail.com>
2013-09-13 14:23:00 +0800

    changed the private vars to public, removed unnecessary methods

--- src/net/dlogic/kryonet/common/entity/Room.java
+++ src/net/dlogic/kryonet/common/entity/Room.java
@@ -4,37 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
-	private int id;
-	private String name;
-	private List<User> userList;
+	public int id;
+	public String name;
+	public List<User> userList;
 	public Room() {
 		userList = new ArrayList<User>();
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public int getId() {
-		return id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public List<User> getUserList() {
-		return userList;
-	}
-	public void setUserList(List<User> userList) {
-		this.userList = userList;
-	}
-	public boolean addUser(User user) {
-		return userList.add(user);
-	}
-	public boolean removeUser(User user) {
-		return userList.remove(user);
-	}
-	public boolean containsUser(User user) {
-		return userList.contains(user);
 	}
 }
