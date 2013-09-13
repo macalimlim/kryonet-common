dae806e8af67a872fe80c4efe539959cb28970cf
macalimlim <macalimlim@gmail.com>
2013-09-13 14:27:53 +0800

    added joinedUser User object

--- src/net/dlogic/kryonet/common/response/JoinRoomSuccessResponse.java
+++ src/net/dlogic/kryonet/common/response/JoinRoomSuccessResponse.java
@@ -1,7 +1,9 @@
 package net.dlogic.kryonet.common.response;
 
 import net.dlogic.kryonet.common.entity.Room;
+import net.dlogic.kryonet.common.entity.User;
 
 public class JoinRoomSuccessResponse {
+	public User joinedUser;
 	public Room joinedRoom;
 }
