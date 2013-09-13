446fb3295faa9292a914577385b104708a20c55a
macalimlim <macalimlim@gmail.com>
2013-09-13 14:23:36 +0800

    removed Connection object, changed the private vars to public

--- src/net/dlogic/kryonet/common/entity/User.java
+++ src/net/dlogic/kryonet/common/entity/User.java
@@ -1,21 +1,7 @@
 package net.dlogic.kryonet.common.entity;
 
-import com.esotericsoftware.kryonet.Connection;
 
 public class User {
-	private String username;
-	private Connection connection;
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public Connection getConnection() {
-		return connection;
-	}
-	public void setConnection(Connection connection) {
-		this.connection = connection;
-	}
-	
+	public int id;
+	public String username;
 }
