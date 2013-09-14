abdbd5be65d4a116595ce22742b0852ccba851ae
macalimlim <macalimlim@gmail.com>
2013-09-14 22:23:36 +0800

    added RoomManagerException

---
+++ src/net/dlogic/kryonet/common/manager/RoomManagerException.java
@@ -0,0 +1,8 @@
+package net.dlogic.kryonet.common.manager;
+
+public class RoomManagerException extends Exception {
+	private static final long serialVersionUID = -8782941805535328959L;
+	public RoomManagerException(String message) {
+		super(message);
+	}
+}
