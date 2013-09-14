66f7b68f7af8df664fa075221688e72ba34d7591
macalimlim <macalimlim@gmail.com>
2013-09-14 22:27:53 +0800

    added methods to add and remove a user to a room, the add method is implemented with checks to see if the room is full and if the room already contains the user

--- src/net/dlogic/kryonet/common/manager/RoomManager.java
+++ src/net/dlogic/kryonet/common/manager/RoomManager.java
@@ -1,7 +1,21 @@
 package net.dlogic.kryonet.common.manager;
 
+import net.dlogic.kryonet.common.constant.ErrorMessage;
 import net.dlogic.kryonet.common.entity.Room;
+import net.dlogic.kryonet.common.entity.User;
 
 public class RoomManager extends BaseManager<Room> {
-	
+	public void addUserToRoom(User user, int roomId) throws RoomManagerException {
+		Room room = get(roomId);
+		if (room.isFull()) {
+			throw new RoomManagerException(ErrorMessage.ROOM_IS_FULL);
+		}
+		if (room.containsUser(user)) {
+			throw new RoomManagerException(ErrorMessage.USER_ALREADY_IN_ROOM);
+		}
+		room.addUser(user);
+	}
+	public void removeUserToRoom(User user, int roomId) {
+		get(roomId).removeUser(user);
+	}
 }
