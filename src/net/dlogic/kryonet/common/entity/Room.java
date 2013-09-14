738a2fda6756a99387c7d4c062988792e8fbf5f6
macalimlim <macalimlim@gmail.com>
2013-09-14 22:24:43 +0800

    made userList private, added methods to add, remove and check user from the list

--- src/net/dlogic/kryonet/common/entity/Room.java
+++ src/net/dlogic/kryonet/common/entity/Room.java
@@ -4,10 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Room {
+	private List<User> userList;
 	public int id;
 	public String name;
-	public List<User> userList;
+	public int maxUsers;
 	public Room() {
 		userList = new ArrayList<User>();
+	}
+	public List<User> getUserList() {
+		return userList;
+	}
+	public boolean addUser(User user) {
+		return userList.add(user);
+	}
+	public boolean removeUser(User user) {
+		return userList.remove(user);
+	}
+	public boolean containsUser(User user) {
+		return userList.contains(user);
+	}
+	public boolean isFull() {
+		return maxUsers == userList.size();
 	}
 }
