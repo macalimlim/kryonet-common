3a044b75d7bfd3a921928b14d0defff7ba2415f9
macalimlim <macalimlim@gmail.com>
2013-09-14 22:23:12 +0800

    added ErrorMessage class for error message constants to be used by various exception classes

---
+++ src/net/dlogic/kryonet/common/constant/ErrorMessage.java
@@ -0,0 +1,10 @@
+package net.dlogic.kryonet.common.constant;
+
+public class ErrorMessage {
+	public static final String SERVER_ALREADY_INITIALIZED = "server already initialized";
+	public static final String SERVER_NOT_INITIALIZED = "server not initialized";
+	public static final String CLIENT_ALREADY_INITIALIZED = "client already initialized";
+	public static final String CLIENT_NOT_INITIALIZED = "client not initialized";
+	public static final String ROOM_IS_FULL = "room is full";
+	public static final String USER_ALREADY_IN_ROOM = "user already in room";
+}
