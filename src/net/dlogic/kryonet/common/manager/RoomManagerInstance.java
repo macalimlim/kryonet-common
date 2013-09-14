4c2eadef669b1bf452b7429471b0a49e701542b1
macalimlim <macalimlim@gmail.com>
2013-09-15 07:30:25 +0800

    made the manager objects public and final

--- src/net/dlogic/kryonet/common/manager/RoomManagerInstance.java
+++ src/net/dlogic/kryonet/common/manager/RoomManagerInstance.java
@@ -2,8 +2,5 @@ package net.dlogic.kryonet.common.manager;
 
 
 public class RoomManagerInstance {
-	private static RoomManager roomManager = new RoomManager();
-	public static RoomManager getInstance() {
-		return roomManager;
-	}
+	public static final RoomManager roomManager = new RoomManager();
 }
--- src/net/dlogic/kryonet/common/manager/UserManagerInstance.java
+++ src/net/dlogic/kryonet/common/manager/UserManagerInstance.java
@@ -2,8 +2,5 @@ package net.dlogic.kryonet.common.manager;
 
 
 public class UserManagerInstance {
-	private static UserManager userManager = new UserManager();
-	public static UserManager getInstance() {
-		return userManager;
-	}
+	public static final UserManager userManager = new UserManager();
 }
