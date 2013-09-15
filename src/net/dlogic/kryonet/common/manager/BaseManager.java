3ceef98639fa173732671429087e8a9076008608
macalimlim <macalimlim@gmail.com>
2013-09-15 10:13:06 +0800

    made the map to public and final, removed unnecessary methods

--- src/net/dlogic/kryonet/common/manager/BaseManager.java
+++ src/net/dlogic/kryonet/common/manager/BaseManager.java
@@ -1,21 +1,8 @@
 package net.dlogic.kryonet.common.manager;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class BaseManager<V> {
-	private Map<Integer, V> map = new HashMap<Integer, V>();
-	public V put(Integer key, V value) {
-		return map.put(key, value);
-	}
-	public V remove(int key) {
-		return map.remove(key);
-	}
-	public V get(int key) {
-		return map.get(key);
-	}
-	public Iterator<V> iterator() {
-		return map.values().iterator();
-	}
+	public final Map<Integer, V> map = new HashMap<Integer, V>();
 }
