64093af0aae8dac0d3f0f43f7402360db05a67a0
macalimlim <macalimlim@gmail.com>
2013-09-17 09:31:03 +0800

    UserManager is now a Integer->User key-value pair

--- src/net/dlogic/kryonet/common/manager/UserManager.java
+++ src/net/dlogic/kryonet/common/manager/UserManager.java
@@ -2,6 +2,6 @@ package net.dlogic.kryonet.common.manager;
 
 import net.dlogic.kryonet.common.entity.User;
 
-public class UserManager extends BaseManager<User> {
+public class UserManager extends BaseManager<Integer, User> {
 	
 }
