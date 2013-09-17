217e9f3f278cc0fcae61a79c0f11a82e73b5b9f3
macalimlim <macalimlim@gmail.com>
2013-09-17 13:28:54 +0800

    added user manager to the Myself object

--- src/net/dlogic/kryonet/common/entity/Myself.java
+++ src/net/dlogic/kryonet/common/entity/Myself.java
@@ -2,7 +2,10 @@ package net.dlogic.kryonet.common.entity;
 
 import net.dlogic.kryonet.common.manager.RoomManager;
 import net.dlogic.kryonet.common.manager.RoomManagerInstance;
+import net.dlogic.kryonet.common.manager.UserManager;
+import net.dlogic.kryonet.common.manager.UserManagerInstance;
 
 public class Myself extends User {
+	public final UserManager userManager = UserManagerInstance.manager;
 	public final RoomManager roomManager = RoomManagerInstance.manager;
 }
